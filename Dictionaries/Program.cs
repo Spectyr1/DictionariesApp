eea381f9760e30c827608d8b441cf7ce137c97d1
Danny Warren <danny@theforbiddenarts.com>
2022-03-24 22:40:13 -0400

    Add project files.

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+Dictionary<string, string> lookup = new Dictionary<string, string>();
+
+lookup["animal"] = "Not a human";
+lookup["fish"] = "Not a human that swims.";
+lookup["human"] = "US";
+
+Console.WriteLine($"The definition of fish is {lookup["fish"]}");
+
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[95] = "Tim Corey";
+employees[28] = "Sue Storm";
+
+Console.WriteLine($"The employee with ID 28 is {employees[28]}");
+
+
+Dictionary<string, int> dayOfWeek = new Dictionary<string, int>();
+
+dayOfWeek["Sunday"] = 1;
+dayOfWeek["Monday"] = 2;
+dayOfWeek["Tuesday"] = 3;
+dayOfWeek["Wednesday"] = 4;
+dayOfWeek["Thursday"] = 5;
+dayOfWeek["Friday"] = 6;
+dayOfWeek["Saturday"] = 7;
+
+Console.WriteLine($"Wednesday is day number {dayOfWeek["Wednesday"]}");
